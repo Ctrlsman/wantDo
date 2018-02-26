3766b421b1a8e6b48153802d6658079c3b18333e
dongsheng.zhang <dongsheng.zhang@rongyg.com.cn>
2018-02-26 18:01:00 +0800

    new project wantDo

---
+++ config/dev.py
@@ -0,0 +1,9 @@
+mysql_cfg = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'root',
+    'password': 'zds819918',
+    'database': 'test',
+    'charset': 'utf-8',
+    'autocommit': True
+}
---
+++ handler/action.py
@@ -0,0 +1,17 @@
+# /usr/bin/python2
+# -*- coding:utf-8 -*-
+
+
+def add_todo():
+    '''
+    添加需要做的事情
+    :return:
+    '''
+    pass
+
+def show_todo():
+    '''
+    展示需要做的事情
+    :return:
+    '''
+    pass
