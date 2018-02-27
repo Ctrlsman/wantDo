002c6f8aa6c9851fcc1ccd50bc8c6511e2af2dc3
dongsheng.zhang <dongsheng.zhang@rongyg.com.cn>
2018-02-27 15:36:10 +0800

    deving...

--- config/dev.py
+++ config/dev.py
@@ -1,8 +1,12 @@
+# -*- coding:utf-8 -*-
+import pymysql
+
 mysql_cfg = {
     'host': '127.0.0.1',
     'port': 3306,
     'user': 'root',
     'password': 'zds819918',
     'database': 'test',
-    'autocommit': True
+    'autocommit': True,
+    'cursorclass': pymysql.cursors.DictCursor,
 }
--- handler/action.py
+++ handler/action.py
@@ -1,17 +1,40 @@
 # /usr/bin/python2
 # -*- coding:utf-8 -*-
+import Tkinter
 
 
-def add_todo():
-    '''
-    添加需要做的事情
-    :return:
-    '''
-    pass
+class App(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.create_panel()
 
-def show_todo():
-    '''
-    展示需要做的事情
-    :return:
-    '''
-    pass
+    def create_panel(self):
+        tk = Tkinter.Tk()
+        tk.title = 'wantDo'
+        tk.geometry('{}x{}'.format(self.width, self.height))
+        tk.mainloop()
+
+    def add_todo(self):
+        '''
+        添加需要做的事情
+        :return:
+        '''
+        pass
+
+    def show_todo(slef):
+        '''
+        展示需要做的事情
+        :return:
+        '''
+        pass
+
+    def del_todo(self):
+        pass
+
+    def save_db(self):
+        '''
+        保存数据到db
+        :return:
+        '''
+        pass
